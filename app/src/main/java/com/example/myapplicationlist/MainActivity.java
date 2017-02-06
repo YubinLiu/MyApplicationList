6c61c7b415b1d7fd9e4add4cfc28e9dd019b870c
Liuyubin <lybin0904@gmail.com>
2017-02-06 22:22:59 +0800

    优化了代码，提高了重用性

--- app/src/main/java/com/example/myapplicationlist/MainActivity.java
+++ app/src/main/java/com/example/myapplicationlist/MainActivity.java
@@ -3,61 +3,52 @@ package com.example.myapplicationlist;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button hotFilm;
-
-    private Button stockEagle;
-
-    private Button xyzReader;
-
-    private Button newest;
-
-    private Button graduation_project;
+    private static Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        hotFilm = (Button) findViewById(R.id.hot_film);
-        stockEagle = (Button) findViewById(R.id.stock_eagle);
-        xyzReader = (Button) findViewById(R.id.xyz_reader);
-        newest = (Button) findViewById(R.id.newest);
-        graduation_project = (Button) findViewById(R.id.graduation_project);
-        hotFilm.setOnClickListener(this);
-        stockEagle.setOnClickListener(this);
-        xyzReader.setOnClickListener(this);
-        newest.setOnClickListener(this);;
-        graduation_project.setOnClickListener(this);
+        //无需用到Button实例,注册监听事件即可
+        findViewById(R.id.hot_film).setOnClickListener(this);
+        findViewById(R.id.stock_eagle).setOnClickListener(this);
+        findViewById(R.id.xyz_reader).setOnClickListener(this);
+        findViewById(R.id.newest).setOnClickListener(this);
+        findViewById(R.id.graduation_project).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.hot_film:
-                Toast.makeText(MainActivity.this,
-                        "启动应用: 热门电影", Toast.LENGTH_SHORT).show();
+                showToast(R.string.hot_film);
                 break;
             case R.id.stock_eagle:
-                Toast.makeText(MainActivity.this,
-                        "启动应用: 股票雄鹰", Toast.LENGTH_SHORT).show();
+                showToast(R.string.stock_eagle);
                 break;
             case R.id.xyz_reader:
-                Toast.makeText(MainActivity.this,
-                        "启动应用: XYZ阅读器", Toast.LENGTH_SHORT).show();
+                showToast(R.string.xyz_reader);
                 break;
             case R.id.newest:
-                Toast.makeText(MainActivity.this,
-                        "启动应用: 最新闻", Toast.LENGTH_SHORT).show();
+                showToast(R.string.newest);
                 break;
             case R.id.graduation_project:
-                Toast.makeText(MainActivity.this,
-                        "启动应用: 毕业设计", Toast.LENGTH_SHORT).show();
+                showToast(R.string.graduation_project);
                 break;
         }
+    }
+
+    private void showToast(int resId) {
+        if (mToast == null) {
+            mToast = Toast.makeText(this, resId, Toast.LENGTH_SHORT);
+        } else {
+            mToast.setText(resId);
+        }
+        mToast.show();
     }
 }
